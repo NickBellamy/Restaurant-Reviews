9fb4b1cd846ee5a7ad70ee7e5e6bda844eed3af1
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-06 09:06:16 +0100

    feat: Stipulate content to cache

---
+++ sw.js
@@ -0,0 +1,26 @@
+const contentToCache = [
+    '/css/style.css',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/js/sw-check.js',
+    'index.html',
+    'restaurant.html'
+]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(v1).then(cache => cache.addAll(contentToCache))
+    );
+});
