1ff4a44b463239f4affbd6f3aea7b17668a1582f
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-06 09:56:06 +0100

    refactor: Tidy up sw_check.js

--- js/sw_check.js
+++ js/sw_check.js
@@ -1,8 +1,11 @@
+// If serviceworker is supported, register sw.js
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(
-            () => console.log('ServiceWorker successfully registered'),
-            error => console.log('ServiceWorker error: ', error)
-        );
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('/sw.js')
+            .then(
+                () => console.log('ServiceWorker successfully registered'),
+                error => console.log('ServiceWorker error: ', error)
+            );
     });
 }
