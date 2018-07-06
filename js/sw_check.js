6256731c5ea11f1c8b525b99722f6ff87feed172
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-06 09:08:45 +0100

    style: Use single quotes in js for consistency

--- js/sw_check.js
+++ js/sw_check.js
@@ -1,8 +1,8 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-        navigator.serviceWorker.register("/sw.js").then(
-            () => console.log("ServiceWorker successfully registered"),
-            error => console.log("ServiceWorker error: ", error)
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(
+            () => console.log('ServiceWorker successfully registered'),
+            error => console.log('ServiceWorker error: ', error)
         );
     });
 }
