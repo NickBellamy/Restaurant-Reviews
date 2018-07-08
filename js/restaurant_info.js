55405c6d37e943297e6084ee945d4b29890ecf22
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-08 10:01:51 +0100

    style: Make quotation marks consistent
    Double quotes are used in HTML, single quotes everywhere else.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -4,7 +4,7 @@ var newMap;
 /**
  * Initialize map as soon as the page is loaded.
  */
-document.addEventListener("DOMContentLoaded", event => {
+document.addEventListener('DOMContentLoaded', event => {
     initMap();
 });
 
@@ -17,22 +17,22 @@ initMap = () => {
             // Got an error!
             console.error(error);
         } else {
-            self.newMap = L.map("map", {
+            self.newMap = L.map('map', {
                 center: [restaurant.latlng.lat, restaurant.latlng.lng],
                 zoom: 16,
                 scrollWheelZoom: false
             });
             L.tileLayer(
-                "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
+                'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}',
                 {
                     mapboxToken:
-                        "pk.eyJ1IjoiZG5hYiIsImEiOiJjamlzbndmdHIxdDBjM3FwbWhkbWIwd3VyIn0.ArSmG0NBpmaqvQu06kKJlA",
+                        'pk.eyJ1IjoiZG5hYiIsImEiOiJjamlzbndmdHIxdDBjM3FwbWhkbWIwd3VyIn0.ArSmG0NBpmaqvQu06kKJlA',
                     maxZoom: 18,
                     attribution:
                         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
                         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
                         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    id: "mapbox.streets"
+                    id: 'mapbox.streets'
                 }
             ).addTo(newMap);
             fillBreadcrumb();
@@ -66,10 +66,10 @@ fetchRestaurantFromURL = callback => {
         callback(null, self.restaurant);
         return;
     }
-    const id = getParameterByName("id");
+    const id = getParameterByName('id');
     if (!id) {
         // no id found in URL
-        error = "No restaurant id in URL";
+        error = 'No restaurant id in URL';
         callback(error, null);
     } else {
         DBHelper.fetchRestaurantById(id, (error, restaurant) => {
@@ -88,19 +88,19 @@ fetchRestaurantFromURL = callback => {
  * Create restaurant HTML and add it to the webpage
  */
 fillRestaurantHTML = (restaurant = self.restaurant) => {
-    const name = document.getElementById("restaurant-name");
+    const name = document.getElementById('restaurant-name');
     name.innerHTML = restaurant.name;
 
-    const address = document.getElementById("restaurant-address");
+    const address = document.getElementById('restaurant-address');
     address.innerHTML = restaurant.address;
 
-    const image = document.getElementById("restaurant-img");
-    image.className = "restaurant-img";
+    const image = document.getElementById('restaurant-img');
+    image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
     image.alt = `${restaurant.name} restaurant`;
     image.title = restaurant.name;
 
-    const cuisine = document.getElementById("restaurant-cuisine");
+    const cuisine = document.getElementById('restaurant-cuisine');
     cuisine.innerHTML = restaurant.cuisine_type;
 
     // fill operating hours
@@ -117,15 +117,15 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 fillRestaurantHoursHTML = (
     operatingHours = self.restaurant.operating_hours
 ) => {
-    const hours = document.getElementById("restaurant-hours");
+    const hours = document.getElementById('restaurant-hours');
     for (let key in operatingHours) {
-        const row = document.createElement("tr");
+        const row = document.createElement('tr');
 
-        const day = document.createElement("td");
+        const day = document.createElement('td');
         day.innerHTML = key;
         row.appendChild(day);
 
-        const time = document.createElement("td");
+        const time = document.createElement('td');
         time.innerHTML = operatingHours[key];
         row.appendChild(time);
 
@@ -137,18 +137,18 @@ fillRestaurantHoursHTML = (
  * Create all reviews HTML and add them to the webpage.
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
-    const container = document.getElementById("reviews-container");
-    const title = document.createElement("h2");
-    title.innerHTML = "Reviews";
+    const container = document.getElementById('reviews-container');
+    const title = document.createElement('h2');
+    title.innerHTML = 'Reviews';
     container.appendChild(title);
 
     if (!reviews) {
-        const noReviews = document.createElement("p");
-        noReviews.innerHTML = "No reviews yet!";
+        const noReviews = document.createElement('p');
+        noReviews.innerHTML = 'No reviews yet!';
         container.appendChild(noReviews);
         return;
     }
-    const ul = document.getElementById("reviews-list");
+    const ul = document.getElementById('reviews-list');
     reviews.forEach(review => {
         ul.appendChild(createReviewHTML(review));
     });
@@ -159,32 +159,32 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  * Create review HTML and add it to the webpage.
  */
 createReviewHTML = review => {
-    const li = document.createElement("li");
+    const li = document.createElement('li');
 
-    const reviewHeader = document.createElement("div");
-    reviewHeader.className = "review-header";
+    const reviewHeader = document.createElement('div');
+    reviewHeader.className = 'review-header';
 
-    const name = document.createElement("span");
-    name.className = "review-name";
+    const name = document.createElement('span');
+    name.className = 'review-name';
     name.innerHTML = review.name;
     reviewHeader.appendChild(name);
 
-    const date = document.createElement("span");
-    date.className = "review-date";
+    const date = document.createElement('span');
+    date.className = 'review-date';
     date.innerHTML = review.date;
     reviewHeader.appendChild(date);
 
     li.appendChild(reviewHeader);
 
-    const reviewContent = document.createElement("div");
-    reviewContent.className = "review-content";
+    const reviewContent = document.createElement('div');
+    reviewContent.className = 'review-content';
 
-    const rating = document.createElement("div");
-    rating.className = "review-rating";
+    const rating = document.createElement('div');
+    rating.className = 'review-rating';
     rating.innerHTML = `RATING: ${review.rating}`;
     reviewContent.appendChild(rating);
 
-    const comments = document.createElement("p");
+    const comments = document.createElement('p');
     comments.innerHTML = review.comments;
     reviewContent.appendChild(comments);
 
@@ -197,11 +197,11 @@ createReviewHTML = review => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant = self.restaurant) => {
-    const breadcrumb = document.querySelector("#breadcrumb ol");
-    const li = document.createElement("li");
-    const link = document.createElement("a");
-    link.setAttribute("href", location.pathname + location.search);
-    link.setAttribute("aria-current", "page");
+    const breadcrumb = document.querySelector('#breadcrumb ol');
+    const li = document.createElement('li');
+    const link = document.createElement('a');
+    link.setAttribute('href', location.pathname + location.search);
+    link.setAttribute('aria-current', 'page');
     link.innerHTML = restaurant.name;
     li.appendChild(link);
     breadcrumb.appendChild(li);
@@ -212,10 +212,10 @@ fillBreadcrumb = (restaurant = self.restaurant) => {
  */
 getParameterByName = (name, url) => {
     if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
+    name = name.replace(/[\[\]]/g, '\\$&');
     const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`),
         results = regex.exec(url);
     if (!results) return null;
-    if (!results[2]) return "";
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 };
